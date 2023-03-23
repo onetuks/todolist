b516000ea3a62784a6bfbeebd7c01f90679a134b
SY97P <onetuks@naver.com>
2023-03-23 23:44:22 +0900

    #6.6 Forms #6.7 Form Validation

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,26 +1,61 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+// * React Hook Form 추가로 폐기
+// function TodoList() {
+//     const [todo, setTodo] = useState("");
+//     const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+//         const {
+//             currentTarget: { value },
+//         } = event;
+//         setTodo(value);
+//         // console.log("onChange -> ", todo)
+//     };
+//     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//         event.preventDefault();
+//         // console.log(todo);
+//     };
+    
+//     return (
+//         <div>
+//             <form onSubmit={onSubmit}>
+//                 <input value={todo} onChange={onChange} placeholder="Write a todo"/>
+//                 <button>Add</button>
+//             </form>
+//         </div>
+//     );
+// }
 
 function TodoList() {
-    const [todo, setTodo] = useState("");
-    const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-        const {
-            currentTarget: { value },
-        } = event;
-        setTodo(value);
-        // console.log("onChange -> ", todo)
+    const { register, handleSubmit, formState } = useForm();
+    const onValid = (data: any) => {
+        console.log("onValid : ", data);
     };
-    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        // console.log(todo);
-    };
-    
+    console.log("Err : ", formState.errors);
+
     return (
-        <div>
-            <form onSubmit={onSubmit}>
-                <input value={todo} onChange={onChange} placeholder="Write a todo"/>
+        <>
+            <form
+                style={{display:"flex", flexDirection: "column"}}
+                onSubmit={handleSubmit(onValid)}
+            >
+                <input {...register("email", {required: true})} placeholder="Email"/>
+                <input {...register("firstName", {required: true})} placeholder="First Name"/>
+                <input {...register("lastName", {required: true})} placeholder="Last Name"/>
+                <input {...register("userName", {required: true})} placeholder="User Name"/>
+                <input {...register("password", {
+                    required: "Password is empty!",
+                    })} placeholder="Password"/>
+                <input {...register("password1", {
+                    required: true,
+                    minLength: {
+                        value: 5,
+                        message: "Your password is too short."
+                    }
+                    })} placeholder="Password1"/>
                 <button>Add</button>
             </form>
-        </div>
+        </>
     );
 }
 export default TodoList;
--- src/index.tsx
+++ src/index.tsx
@@ -9,9 +9,11 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <>
     <ThemeProvider theme={theme}>
       <RouterProvider router={Router} />
     </ThemeProvider>
-  </React.StrictMode>
+  </>
+  // </React.StrictMode>
 );
