e6eb32f44979f1cc1609c1d8d2405318660dab0e
SY97P <onetuks@naver.com>
2023-03-23 23:13:49 +0900

    renewal commit

---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+    bgColor: "#636e72",
+    textColor: "#dfe6e9",
+    accentColor: "#fdcb6e",
+    btnColor: "#fdcb6e"
+}
