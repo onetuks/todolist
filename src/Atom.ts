cce56722608574aa3864417e744c5847f18bd339
SY97P <onetuks@naver.com>
2023-03-24 21:17:40 +0900

    #6.16 Selectors part 1 - Recoil > selector - atom 과 비슷한 역할

--- src/Atom.ts
+++ src/Atom.ts
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export interface ITodo {
     text: string;
@@ -10,3 +10,15 @@ export const todoState = atom<ITodo[]>({
   key: "todos",
   default: [],
 });
+
+export const todoSelector = selector({
+    key: "todoSelector",
+    get: ({get}) => {
+        const todos = get(todoState);
+        return [
+            todos.filter((todo) => todo.category === "TODO"),
+            todos.filter((todo) => todo.category === "DOING"),
+            todos.filter((todo) => todo.category === "DONE"),
+        ]
+    },
+})
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,23 +1,40 @@
 import { useRecoilValue } from "recoil";
-import { todoState } from "../Atom";
+import { todoSelector, todoState } from "../Atom";
 import CreateTodo from "./CreateTodo";
 import Todo from "./Todo";
 
 function TodoList() {
-  const todos = useRecoilValue(todoState);
-  console.log(todos);
+  const [todos, doings, dones] = useRecoilValue(todoSelector);
 
   return (
     <>
       <h1>To do</h1>
       <hr />
       <CreateTodo />
+      <h2>TODO</h2>
       <ul>
         {todos.map((todo) => (
           // {...todo} 보내주는 곳과 사용하는 곳에서 todo에 대한 interface가 같으므로 이렇게 내부 원소를 보내줘도 됨.
           <Todo key={todo.id} {...todo} />
         ))}
       </ul>
+      <hr />
+      <h2>DOING</h2>
+      <ul>
+        {doings.map((doing) => (
+          // {...todo} 보내주는 곳과 사용하는 곳에서 todo에 대한 interface가 같으므로 이렇게 내부 원소를 보내줘도 됨.
+          <Todo key={doing.id} {...doing} />
+        ))}
+      </ul>
+      <hr />
+      <h2>DONE</h2>
+      <ul>
+        {dones.map((done) => (
+          // {...todo} 보내주는 곳과 사용하는 곳에서 todo에 대한 interface가 같으므로 이렇게 내부 원소를 보내줘도 됨.
+          <Todo key={done.id} {...done} />
+        ))}
+      </ul>
+      <hr />
     </>
   );
 }
